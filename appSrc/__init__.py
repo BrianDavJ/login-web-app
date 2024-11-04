e3f50b9227dde47447274b641529d4ddee9449db
bdj <brian.d.jaime97@gmail.com>
2024-11-04 18:53:14 -0300

    refresh de página y modularizacion

--- appSrc/__init__.py
+++ appSrc/__init__.py
@@ -8,12 +8,9 @@ def create_app():
     CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST"]}})
     app.config['CORS_HEADERS'] = 'Content-Type'
     app.config['SECRET_KEY']='placerHolderKey'
-    
-    
 
     from .auth import auth
 
     app.register_blueprint(auth,url_prefix='/')
-
 
     return app
